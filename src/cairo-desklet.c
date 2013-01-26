426d4b386590ff5425016df113b237fe1b309da1
Matthieu Baerts <matttbe@gmail.com>
2013-01-26 16:05:13 +0100

    Init: typo: g_type_init is not needed since GLib 2.35 (the unstable version of 2.36)

--- src/cairo-desklet.c
+++ src/cairo-desklet.c
@@ -124,12 +124,12 @@ static gboolean _start_delayed (gpointer *data)
 
 int main (int argc, char** argv)
 {
-	#if (GLIB_MAJOR_VERSION == 2 && GLIB_MINOR_VERSION < 26) // no longer needed now...
+	#if !defined (GLIB_VERSION_2_36) // no longer needed now... (>= 2.35)
 	g_type_init (); // should initialise threads too on new versions of GLIB (>= 2.24)
-	#if (GLIB_MINOR_VERSION < 24)
+	#endif
+	#if (GLIB_MAJOR_VERSION == 2 && GLIB_MINOR_VERSION < 24)
 	if (!g_thread_supported ())
 		g_thread_init (NULL);
-	#endif
 	#endif
 	dbus_g_thread_init ();
 
